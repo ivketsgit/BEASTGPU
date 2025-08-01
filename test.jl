e42fc0db54f299f719ecd641141cb70e6ac57f1d
ivkets <ian.vankets@UGent.be>
2025-08-01 12:53:03 +0200

    test

--- test.jl
+++ test.jl
@@ -1,12 +1,18 @@
 using CUDA
 
-@show CUDA.device()
+# @show CUDA.device()
 
-device = CUDA.device()
-println("Device ID: ", device)
-println("Device Name: ", CUDA.name(device))
+# device = CUDA.device()
+# println("Device ID: ", device)
+# println("Device Name: ", CUDA.name(device))
 
-for i in 0:CUDA.device_count()-1
-    dev = CuDevice(i)
-    println("Device $i: ", CUDA.name(dev))
-end
+# for i in 0:CUDA.device_count()-1
+#     dev = CuDevice(i)
+#     println("Device $i: ", CUDA.name(dev))
+# end
+
+t = @elapsed begin
+    CUDA.pin(Array{ComplexF64}(undef, Int(round(38400/1.5)),38400))
+    CuArray()
+end 
+@show t
