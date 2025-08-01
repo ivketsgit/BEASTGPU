d75e3c315a78b4b7ddce6638fc5b2c56df15d4b1
ivkets <ian.vankets@UGent.be>
2025-08-01 15:01:27 +0200

    data

--- data/other/time_pinned.jl
+++ data/other/time_pinned.jl
@@ -2,25 +2,25 @@ using CUDA
 using BenchmarkTools
 
 samples = 100
-for e in range(1, 10)
+for e in range(7, 10)
     times = []
     for i in 1:samples
         GC.gc()
         t = @elapsed begin
             CUDA.pin(Array{ComplexF64}(undef, Int(round(38400*(e/10))),38400))
         end
+        push!(times, t)
         if i % 10 == 0
             print(".")
         end
     end 
-    push!(times, t)
 
     GC.gc()
 
 
     num_runs = length(times)
     total_duration = sum(times)
-    open("data/other/pinned_server/$(e).txt", "a") do file
+    open("data/other/pinned_home_pc/$(e).txt", "a") do file
         println(file, """
                 Manual Benchmark of duration $(total_duration) seconds over $(num_runs) runs:"
                 $(times)
