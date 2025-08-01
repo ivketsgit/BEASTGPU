324d0f6622330603a601847b3b835a1402ecc83b
ivketsgit <ian.vankets@email.com>
2025-08-01 18:42:43 +0200

    data

--- data/graph_data.jl
+++ data/graph_data.jl
@@ -1,7 +1,7 @@
 
 global density_values = [24, 30, 33, 36, 38, 40]
 global intgral_amount = [764411904, 1866240000, 2732361984, 3869835264, 4804153344, 5898240000]
-global system_matrix_size = [191158276, 466646404, 683195044, 967583236, 1201176964, 1474713604]
+global system_matrix_size = [191158276, 466646404, 683195044, 967583236, 1201176964, 1474713604] # in elements, * 16 of Bytes
 # for i in 1:5
 #     println(log10(graph_elements[i] - graph_elements[i+1]))
 # end
