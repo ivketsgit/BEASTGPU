4741aed19e1890d89f5341b4748e39832d9f5fca
ivkets <ian.vankets@UGent.be>
2025-08-01 14:01:06 +0200

    test

--- test.jl
+++ test.jl
@@ -20,7 +20,7 @@ t = @elapsed begin
     # CUDA.pin(Array{ComplexF64}(undef, 38400, 38400))
     # CuArray()
 
-    CUDA.pin(Array{ComplexF64}(undef, Int(round(38400)),38400))
+    CUDA.pin(Array{ComplexF64}(undef, Int(round(38400*0.5)),38400))
 end 
 @show t
 GC.gc()
