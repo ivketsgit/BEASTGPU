3199baf204487deff93b399ef7897b6c7a23f375
ivketsgit <ian.vankets@email.com>
2025-08-03 10:37:21 +0200

    data

---
+++ data/other/time_transfer_with_pinned_server.jl
@@ -0,0 +1,40 @@
+using CUDA
+using BenchmarkTools
+
+include("../graph_data.jl")
+
+
+samples = 100
+for e in system_matrix_size
+    times = []
+
+    A = rand(ComplexF64, e)
+    d_array = CuArray(A) 
+    
+    CPU_array = CUDA.pin(Array{ComplexF64}(undef,  e))
+
+    GC.gc()
+
+
+
+
+    # Benchmark
+    times = @benchmark begin
+        copyto!($CPU_array, $d_array)
+    end samples=samples evals=1 seconds=3600 * 2
+
+
+    # times = @benchmark copyto!($CPU_array, $d_array)  samples=samples evals=1 seconds=3600 * 2
+
+    GC.gc()
+
+    times = times.times / 1e9 
+    num_runs = length(times)
+    total_duration = sum(times) 
+    open("data/other/transfer_with_pinned_server/$(e).txt", "a") do file
+        println(file, """
+                Manual Benchmark of GPU-to-CPU transfer duration $(total_duration) seconds over $(num_runs) runs:
+                $(times)
+                """)
+    end
+end
