060f849cf92f9f4214fae3368214bde31c8f3eee
ivkets <ian.vankets@UGent.be>
2025-08-04 23:48:24 +0200

    data

--- data/graph_data.jl
+++ data/graph_data.jl
@@ -2,7 +2,7 @@
 global density_values = [24, 30, 33, 36, 38, 40]
 global intgral_amount = [764411904, 1866240000, 2732361984, 3869835264, 4804153344, 5898240000]
 global system_matrix_size = [191158276, 466646404, 683195044, 967583236, 1201176964, 1474713604] # in elements, * 16 of Bytes
-global partial_store_threads = [4, 8, 9, 10, 11, 12, 14, 15, 16]
+global partial_store_threads = [4, 8, 9, 10, 11, 12, 14, 16]
 global partial_store_mem = [1, 6, 12, 18, 24, 30]#[8, 12, 16, 20, 24, 28, 32]
 # for i in 1:5
 #     println(log10(graph_elements[i] - graph_elements[i+1]))
